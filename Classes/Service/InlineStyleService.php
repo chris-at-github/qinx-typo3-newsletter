c528c50f842f789a35cb20b34f9e553c13e495f7
Chris <pschorr.christian@gmail.com>
2016-09-14 04:53:13 +0200

    Replace styles only if you are in newsletter folder

--- Classes/Service/InlineStyleService.php
+++ Classes/Service/InlineStyleService.php
@@ -11,6 +11,49 @@ namespace Qinx\Qxnewsletter\Service;
 class InlineStyleService {
 
 	/**
+	 * objectManager
+	 *
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManager
+	 */
+	protected $objectManager;
+
+	/**
+	 * Plugin TypoScript Settings
+	 *
+	 * @var array $settings
+	 */
+	protected $settings;
+
+	/**
+	 * return an instance of objectManager
+	 *
+	 * @param none
+	 * @return \TYPO3\CMS\Extbase\Object\ObjectManager
+	 */
+	public function getObjectManager() {
+		if(($this->objectManager instanceof \TYPO3\CMS\Extbase\Object\ObjectManager) === false) {
+			$this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
+		}
+
+		return $this->objectManager;
+	}
+
+	/**
+	 * Return the typoscript settings for this plugin
+	 *
+	 * @return array
+	 */
+	public function getSettings() {
+		if(isset($this->settings) === false) {
+			$this->settings = $this->getObjectManager()->get('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager')->getConfiguration(
+				\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS, 'Qxnewsletter', 'Frontend'
+			);
+		}
+
+		return $this->settings;
+	}
+
+	/**
 	 * After render hook for non cached pages
 	 *
 	 * @param array $params
@@ -37,8 +80,17 @@ class InlineStyleService {
 	 * @return string
 	 */
 	protected function transform($pageSource) {
+		$settings = $this->getSettings();
+		$rootline = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Utility\RootlineUtility', $GLOBALS['TSFE']->page['pid'])->get();
 		$emogrifier = new \Pelago\Emogrifier($pageSource);
 
-		return $emogrifier->emogrify();
+		// Replace styles only if you are in newsletter folder
+		foreach($rootline as $page) {
+			if((int) $page['uid'] === (int) $settings['rootPid']) {
+				return $emogrifier->emogrify();
+			}
+		}
+
+		return $pageSource;
 	}
 }
